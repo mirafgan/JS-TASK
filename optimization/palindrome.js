c3af62d632ed43089f607a166ad8f66291fa995c
mirafgan <mirefqan1999@gmail.com>
2023-05-02 09:59:00 +0400

    palindrome optimization

--- optimization/palindrome.js
+++ optimization/palindrome.js
@@ -1,15 +1,10 @@
 
-let str = 'hanna'
+let str = 'hannah'
 function palindrome(str) {
-    let part1 = '';
-    let part2 = '';
     let mid = Math.floor(str.length / 2);
-    for (let i = 0; i < str.length; i++) {
-        if (part1[i] !== part2[i]) return false;
-        if (i == mid && str.length % 2) continue
-        if (i < mid) part1 += str[i];
-        else part2 = str[i] + part2;
+    for (let i = 0; i < mid; i++) {
+        if (str[i] != str[str.length - 1 - i]) return false;
     }
-    return part1 == part2 ?  true : false;
+    return true;
 }
 palindrome(str)
